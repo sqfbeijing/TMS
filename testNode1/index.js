5b3dff677684c1f3c4b4863a9b2692486d505d64
Giovanni <854413241@qq.com>
2016-05-11 17:32:40 +0800

    从Git删掉无用文件

--- testNode1/index.js
+++
@@ -1,9 +0,0 @@
-// var debugger = require('debugger');
-
-
-x = 5;
-setTimeout(function () {
-  debugger;
-  console.log("world");
-}, 1000);
-console.log("hello");
--- todoist-express-prototype/test.js
+++
@@ -1,3 +0,0 @@
-var path = require("path");
-console.log(". = %s", path.resolve("."));
-console.log("__dirname = %s", path.resolve(__dirname));
